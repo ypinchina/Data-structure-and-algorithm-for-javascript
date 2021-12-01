3126d1f4720f501b3816cc8bb452e96df3c16b42
ypinchina <batmanyp@foxmail.com>
2021-12-01 16:10:46 +0800

    map的基础知识与leetcode 349使用map实现

---
+++ map/intersection-of-two-arrays.ts
@@ -0,0 +1,27 @@
+//349. 两个数组的交集 https://leetcode-cn.com/problems/intersection-of-two-arrays/
+// 给定两个数组，编写一个函数来计算它们的交集。
+
+// 示例 1：
+
+// 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+// 输出：[2]
+// 示例 2：
+
+// 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// 输出：[9,4]
+
+// 使用map 字典来解决这个问题
+function intersection(nums1: number[], nums2: number[]): number[] {
+  let map = new Map()
+  nums1.forEach(item => map.set(item, true))
+  let arr = []
+  nums2.forEach(item => {
+    if (map.get(item)) {
+      arr.push(item)
+      map.delete(item)
+    }
+  })
+  return arr
+}
+
+// 时间复杂度O(N^2) 空间复杂度O(N)
---
+++ map/map.js
@@ -0,0 +1,17 @@
+let m1 = new Map()
+// 增
+m1.set('a', 'aaa')
+m1.set('b', 'bbbbb')
+
+// 删
+
+m1.delete('a')
+// 全删 
+m1.clear()
+
+// 改
+
+m1.set('b', 222222)
+
+// 查
+m1.get('b')
