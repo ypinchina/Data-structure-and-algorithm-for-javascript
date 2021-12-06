d1759cd6aae820ad1232c026c06c2e59bdde75c7
ypinchina <batmanyp@foxmail.com>
2021-12-06 12:26:41 +0800

    原型链相关知识和面试题

---
+++ linkedList/proto.js
@@ -0,0 +1,28 @@
+// 原型链相关知识
+let object = {}
+let fun = () => {}
+let arr = []
+
+object.__proto__ === Object.prototype // true
+object.__proto__.__proto__ === null // true
+fun.__proto__  === Function.prototype // true
+fun.__proto__.__proto__ === Object.prototype // true
+arr.__proto__ === Array.prototype // true
+arr.__proto__.__proto__ === Object.prototype // true
+
+
+// 原型链知识点
+
+// 1. 如果A沿着原型链能找到B.prototype,那么A instanceof B 为true
+
+
+// 使用场景 
+arr instanceof Array  // true  用于判断是否为数组
+arr instanceof Object // true
+fun instanceof Function // true
+
+// 2. 如果在A对象上没有找到x属性，那么会去沿着原型链去找x属性
+
+Function.prototype.x = 'x'
+
+fun.x === 'x' // true
